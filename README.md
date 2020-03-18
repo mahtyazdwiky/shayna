72266c04f701b06ce80dc44dc9e917c50d166fc0
Mahtyaz Dwiky <mahtyaz89@gmail.com>
2020-03-19 00:05:10 +0700

    upload projek vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # shayna
-E-commerce LaravelVueJS (Shayna)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroCarousel />
+    <ProductCarousel />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroCarousel from "@/components/HeroCarousel.vue";
+import ProductCarousel from "@/components/ProductCarousel.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroCarousel,
+    ProductCarousel,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+};
+</script>
